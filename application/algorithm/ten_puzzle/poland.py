15dfd234784b352f6d49069954cfe55f3878b751
taku ikegami <takuron1996@gmail.com>
2023-06-07 23:54:10 +0900

    逆ポーランド記法から通常数式に変換

--- application/algorithm/ten_puzzle/poland.py
+++ application/algorithm/ten_puzzle/poland.py
@@ -36,6 +36,21 @@ class Operator(Enum):
                 return operator.value[1]
         return None
 
+    @classmethod
+    def is_operator(cls, value):
+        """演算子かどうかを判定
+
+        Args:
+            value (str): 演算子
+
+        Returns:
+            bool: 演算子ならTrue
+        """
+        for operator in cls:
+            if operator.value[0] == value:
+                return True
+        return False
+
 
 def calc_poland(exp: str):
     """逆ポーランド記法
@@ -52,10 +67,39 @@ def calc_poland(exp: str):
         if "0" <= e <= "9":
             results.append(int(e))
         else:
-            operator_method = Operator.get_operator_method(e)
-            if operator_method is None:
+            if not Operator.is_operator(e):
                 raise Exception("数字と演算子以外は入力しないでください")
+            operator_method = Operator.get_operator_method(e)
             num1 = results.pop()
             num2 = results.pop()
             results.append(operator_method(num2, num1))
+    return results.pop()
+
+
+def decode_poland(exp: str):
+    """逆ポーランド記法から通常の計算式を復元
+
+    Args:
+        exp (str): 逆ポーランド記法の数式
+
+    Returns:
+        str: 通常の計算式
+    """
+    results = []
+
+    for e in exp:
+        if "0" <= e <= "9":
+            results.append(e)
+        else:
+            if not Operator.is_operator(e):
+                raise Exception("数字と演算子以外は入力しないでください")
+            e1 = results.pop()
+            e2 = results.pop()
+
+            if e in ("*", "/"):
+                if len(e1) > 1:
+                    e1 = f"({e1})"
+                if len(e2) > 2:
+                    e2 = f"({e2})"
+            results.append(e2 + e + e1)
     return results.pop()
--- application/tests/ten puzzle/test_poland.py
+++ application/tests/ten puzzle/test_poland.py
@@ -1,6 +1,6 @@
 import pytest
 
-from algorithm.ten_puzzle.poland import calc_poland
+from algorithm.ten_puzzle.poland import calc_poland, decode_poland
 
 
 def test_calc_poland1():
@@ -14,4 +14,18 @@ def test_calc_poland2():
     exp = "612+a*8-"
     with pytest.raises(Exception) as e:
         calc_poland(exp)
+    assert str(e.value) == "数字と演算子以外は入力しないでください"
+
+
+def test_decode_poland1():
+    """正常動作"""
+    exp = "612+*8-"
+    assert decode_poland(exp) == "6*(1+2)-8"
+
+
+def test_decode_poland1():
+    """数字と演算子以外の異常系動作"""
+    exp = "612+a*8-"
+    with pytest.raises(Exception) as e:
+        decode_poland(exp)
     assert str(e.value) == "数字と演算子以外は入力しないでください"
